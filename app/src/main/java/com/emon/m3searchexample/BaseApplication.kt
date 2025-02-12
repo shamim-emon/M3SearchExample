8ef7be3b86521d4403d02fe778b0f20b7e52544d
shamim-emon <emon.dev.bd@gmail.com>
2025-02-13 01:28:59 +0600

    feat: Initial project setup with Hilt and Compose
    This commit initializes the project structure and dependencies, including:
    
    -   Added `.gitignore` files for the project root, `.idea`, and app module.
    -   Created `AndroidManifest.xml` with basic application configuration.
    -   Implemented `backup_rules.xml` and `data_extraction_rules.xml` for data backup.
    -   Configured `build.gradle.kts` for the app and root project, including Android, Kotlin, Compose, and Hilt plugins and dependencies.
    -   Created `Color.kt`, `Theme.kt`, and `Type.kt` for UI theming.
    -   Added `colors.xml`, `themes.xml`, and `strings.xml` for resource management.
    -   Initialized `ExampleInstrumentedTest.kt` and `ExampleUnitTest.kt` for testing.
    -   Added `gradle.properties` and `libs.versions.toml` for dependency and project configuration.
    -   Created `gradle-wrapper.properties`, `gradlew`, and `gradlew.bat` for Gradle wrapper setup.
    -   Included basic `ic_launcher` resources for the app icon.
    -   Set up `MainActivity.kt` as the main entry point with a basic Compose UI.
    -   Implemented BaseApplication class.
    - Added project configuration files like `compiler.xml`, `deploymentTargetSelector.xml`, `gradle.xml`, `kotlinc.xml`, `migrations.xml`, `misc.xml`, `runConfigurations.xml`, and `vcs.xml`
    - added `proguard-rules.pro` file.
    - added various launcher icon in different dpi.

---
+++ app/src/main/java/com/emon/m3searchexample/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.emon.m3searchexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application()  {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
